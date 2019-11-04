201f46da20458ffa12fd6e38a6b9857f38fa3abc
jerni81 <jerni81@yahoo.com>
2019-11-04 18:51:49 -0500

    add styling footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const Footer = props => {
   return (
     <div className="footer">
-      footer
+        <FontAwesomeIcon icon={faLinkedin} size="2x"/>
+        @AJJernigan
+        <FontAwesomeIcon icon={faGithub} size="2x"/>
     </div>
   );
 };
