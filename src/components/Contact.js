c0adad202846438972fee5da886278c691699228
jerni81 <jerni81@yahoo.com>
2019-11-05 14:23:42 -0500

    set up hooks in contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 
 const Contact = props => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  const handleName = e => {
+      setName(e.target.value)
+  }
+  const handleEmail = e => {
+      setEmail(e.target.value)
+  }
+  const handleMessage = e => {
+      setMessage(e.target.value)
+  }
+
   return (
     <div className="contact">
       <h1><u>Contact</u></h1>
       <form>
         <p>Have a question or want to work together?</p>
-        <input type="text" placeholder="name"/>
-        <input type="email" placeholder="email"/>
-        <textarea rows="14" cols="40" wrap="hard" id="message" placeholder="message"></textarea>
-        <input type="submit" id="submit"/>
+        <input
+          name="name"
+          type="text"
+          placeholder="name"
+          onChange={handleName}
+        />
+        <input
+          name="email"
+          type="email"
+          placeholder="email"
+          onChange={handleEmail}
+        />
+        <textarea
+          name="message"
+          rows="14"
+          cols="40"
+          wrap="hard"
+          id="message"
+          placeholder="message"
+          onChange={handleMessage}
+        >
+        </textarea>
+        <input
+          name="submit"
+          type="submit"
+          id="submit"
+        />
       </form>
     </div>
   );
