888a35829229ccaa2b8be9840465f506d996c299
jerni81 <jerni81@yahoo.com>
2019-11-05 10:37:59 -0500

    style message box

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,7 @@ const Contact = props => {
         <p>Have a question or want to work together?</p>
         <input type="text" placeholder="name"/>
         <input type="email" placeholder="email"/>
-        <input type="text" placeholder="message" id="message"/>
+        <textarea rows="14" cols="40" wrap="hard" id="message" placeholder="message"></textarea>
         <input type="submit" id="submit"/>
       </form>
     </div>
