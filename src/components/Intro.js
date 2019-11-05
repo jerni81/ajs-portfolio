30704feaf5bd893cba63ee5b2625ef4923d2c1a6
jerni81 <jerni81@yahoo.com>
2019-11-05 10:54:01 -0500

    style name

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,7 +4,11 @@ import { Link } from "react-router-dom";
 const Intro = props => {
   return (
     <div className="intro">
-      <h1>Intro</h1>
+      <h1 id="name">AJ Jernigan</h1>
+      <div className="title">
+        <h1>Software</h1>
+        <h1>Engineer</h1>
+      </div>
     </div>
   );
 };
