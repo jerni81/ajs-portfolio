1bae57b6d02f9ed8ed823fe0932ac5104bc4b675
jerni81 <jerni81@yahoo.com>
2019-11-07 14:25:57 -0500

    style bio hover

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -7,14 +7,20 @@ const Bio = props => {
       <h1><u>Bio</u></h1>
       <div className="bio-wrapper">
         <div className="statement">
+          <h3>Personal Statement</h3>
           <div className="shwStatement">
+            <p>
+              I am a tenacious software engineer, who enjoys cultivating and environment of evolution and growth. With many years in sales and leadership positions, my teamwork and communication skills are excellent. I enjoy a good challenge and am a very methodical problem solver. Something that is very important to me is having a positive impact on those around me and the world at large.
+            </p>
           </div>
         </div>
         <div className="tech">
+          <h3>Tech Stack</h3>
           <div className="shwTech">
           </div>
         </div>
         <div className="school">
+          <h3>Education</h3>
           <div className="shwSchool">
           </div>
         </div>
