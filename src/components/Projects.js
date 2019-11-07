8064c7788259820e1172d430db414a30d793fdf6
jerni81 <jerni81@yahoo.com>
2019-11-07 16:46:15 -0500

    make project api call

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios';
+
+
 
 const Projects = props => {
+  const [cards, setCards] = useState()
+
+  const getProjects = async() => {
+    const drive = '1dtenl7Cc9R_UcKxnYvCeOFT9IaOwFw46JcxnquZYR_k'
+    const data = await axios.get(`https://spreadsheets.google.com/feeds/list/${drive}/od6/public/values?alt=json`);
+    const projects = data.data.feed.entry.map((d, i) => {
+      let project = {
+      name: d.gsx$title.$t,
+      link: d.gsx$link.$t,
+      img: d.gsx$image.$t
+    }
+    return project
+    })
+    setCards(projects)
+  }
+
+const display = cards && cards.map((d,i) => {
+  return (
+    <div className="project">
+    {d.name}
+    </div>
+  )
+})
+
+  useEffect(()=>{
+      getProjects();
+  })
+
   return (
     <div className="projects">
       <h1><u>Projects</u></h1>
+      <div className='projectWrapper'>
+        {display}
+      </div>
     </div>
   );
 };
