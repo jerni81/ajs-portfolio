30e505bc1816a28b97ff67e62d92f9b7e88eaf0b
jerni81 <jerni81@yahoo.com>
2019-11-18 16:10:00 -0500

    update bg img

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,7 +3,7 @@ import React from "react";
 const Intro = props => {
   return (
     <div className="intro">
-      <h1 id="name">AJ Jernigan</h1>
+      <h1 id="name">A.J. Jernigan</h1>
       <div className="title">
         <h1>Software</h1>
         <h1>Engineer</h1>
